6151b5a4c2f295bac90a67a0c8a0ebb77f9ae0e9
RyujiOdaJP <ryuji.oda@gmail.com>
2020-02-13 15:18:29 +0700

    left memo which attempted

--- ryuji.oda19/week04/search.py
+++ ryuji.oda19/week04/search.py
@@ -1,4 +1,4 @@
-import requests,bs4
+import requests,bs4,re,datetime
 #パース対象サイトをここに追加
 targetSite =['AtCoder']
 
@@ -8,9 +8,9 @@ class GetHtml:
     """
     # 検索サイトのURL登録
     def __init__(self):
-        self.headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0",}
+        self.headers = {"User-Agent": "Google Chrome/Version 80.0.3987.87 (Official Build) (64-bit)",}
         self.urls = {
-            targetSite[0]:'https://atcoder.jp/home?lang=en'
+            targetSite[0]:'https://atcoder.jp/contests/'
         }
 
     # 対象ページのHtml全文を取得
@@ -29,6 +29,8 @@ class ParseHtml:
     # 企業名と評価ポイントの取得
     def parseNamePoint(self, textList):
         #パース用のタグ登録
+        # contest_table = 'contest-table-upcoming > div > table'
+
         table = {
             targetSite[0]:["div", "contest-table-upcoming"]
         }
@@ -41,7 +43,11 @@ class ParseHtml:
         #     # targetSite[1]:["span", "point"],
         # }
 
-        contestnamePoint = {}
+        contest_name = []
+        time = []
+        str_time = []
+        contest_name_time = ''
+
         for site in targetSite[:1]:
             try:
                 # 会社評価ポイントの取得
@@ -51,17 +57,37 @@ class ParseHtml:
                 # parseCname =  textList[site].find(nameTag[site][0], class_=nameTag[site][1])
                 # cname = parseCname.getText().replace('\n','').replace(' ', '')
                 parseTable = textList[site].find(table[site][0], id=table[site][1])
-                cTable = parseTable.getText().replace('\n', '').replace(' ', '')
+                time = re.findall('\d{4}-\d{2}-\d{2} \d+:\d+:\d+',str(parseTable))
+                # time = parseTable.findAll('time')
+                # a_tag = parseTable.findAll('a')
+                # link_element = a_tag['href']
+                # for i in range(len(time)):
+                #     href = 	link_element['href']
+                # contest_name = parseTable.findAll('a')
+                contest_name = re.findall('contests[\w\d/%#$&?()~_.=+-]+',str(parseTable))
+                # contest_name_str = contest_name.string
+
+                cTable = parseTable.getText().replace('\n','').split()
+                # parse_contest = textList[site].find(contest_table)
+                # cContest = parse_contest.getText().split('\n')
+
+
+                print(parseTable,'\n')
+                print(time)
+                print(contest_name)
 
             # 検索結果が無かった場合の処理
             except AttributeError:
-                contestnamePoint.setdefault(site, ['None','None'])
+                contest_name_time = None,None
+
 
             # 検索結果が有った場合の処理
-            else:
-                contestnamePoint.setdefault(site, [cTable]) #,cname, cpoint
+        for i in range(len(time)):
+            str_time.append(datetime.datetime.strptime(time[i], '%Y-%m-%d %H:%M:%S'))
+            print(str_time)
+            contest_name_time += contest_name[i] + ':\n' + time[i] +'(JPtime +0900)\n\n'
 
-        return contestnamePoint
+        return contest_name_time
 
 # print(GetHtml().getText())
 print(ParseHtml().parseNamePoint(GetHtml().getText()))
