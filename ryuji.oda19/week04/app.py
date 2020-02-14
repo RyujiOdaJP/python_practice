b6ec0bec783557d51c130b5e06f60f0ad02f4af1
RyujiOdaJP <ryuji.oda@gmail.com>
2020-02-14 12:15:47 +0700

    left memo which attempted

--- ryuji.oda19/week04/app.py
+++ ryuji.oda19/week04/app.py
@@ -1,18 +1,30 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template
+from search import GetHtml,ParseHtml
+
+from flask import Markup
+
 app = Flask(__name__)
 
-@app.route('/')
-@app.route("/index")
-def index():
-    name = request.args.get('name')
-    word = ['red', 'blue', 'green']
-    return render_template("index.html", name=name, word=word)
 
-@app.route('/index',methods=['post'])
-def post():
-    name = request.form['name']
-    word = ['red', 'blue', 'green']
-    return render_template('index.html',name=name, word=word)
+
+@app.route('/')
+# @app.route("/index")
+def index():
+    # name = request.args.get('name')
+    # word = ['red', 'blue', 'green']
+    html = GetHtml().getText()
+    parse = ParseHtml().parseName_Date(html)
+    info = ''
+    for i in range(len(parse)):
+        info += '<h1>' + parse[i] + '</h1><br>'
+
+    return render_template("index.html", parse=Markup(info))
+
+# @app.route('/index',methods=['post'])
+# def post():
+#     name = request.form['name']
+#     word = ['red', 'blue', 'green']
+#     return render_template('index.html',name=name, word=word)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- ryuji.oda19/week04/search.py
+++ ryuji.oda19/week04/search.py
@@ -6,20 +6,20 @@ class GetHtml:
     """
     GetHtml as text
     """
-    # 検索サイトのURL登録
+    # 検索サイトのURL登録 #searching target
     def __init__(self):
         self.headers = {"User-Agent": "Google Chrome/Version 80.0.3987.87 (Official Build) (64-bit)",}
         self.urls = {
             targetSite[0]:'https://atcoder.jp/contests/'
         }
 
-    # 対象ページのHtml全文を取得
+    # 対象ページのHtml全文を取得 #get whole html
     def getText(self):
         textList = {}
         for url in self.urls:
             res = requests.get(self.urls[url], headers=self.headers)
-            text = bs4.BeautifulSoup(res.text, "html.parser")
-            textList.setdefault(url, text)
+            text = bs4.BeautifulSoup(res.text, "html.parser") #enable to select html tag and attribute by beautifulsoup
+            textList.setdefault(url, text) #store whole html in variable
         return textList
 
 class ParseHtml:
@@ -27,72 +27,40 @@ class ParseHtml:
     ParseHtmlHtml to get required values
     """
     # 企業名と評価ポイントの取得
-    def parseNamePoint(self, textList):
+    def parseName_Date(self, textList):
         #パース用のタグ登録
-        # contest_table = 'contest-table-upcoming > div > table'
-
         table = {
             targetSite[0]:["div", "contest-table-upcoming"]
         }
-        # dateTag =  {
-        #     targetSite[0]:["h3", "fs-18 lh-1o3 p-r"] #[tag, class]
-        #     # targetSite[1]:["h2", "companyName"],
-        # }
-        # nameTag = {
-        #     targetSite[0]:["p", "totalEvaluation_item fs-15 fw-b"]
-        #     # targetSite[1]:["span", "point"],
-        # }
 
         contest_name = []
         time = []
         str_time = []
         modified_2H = []
-        contest_name_time = ''
+        contest_name_time = []
 
         for site in targetSite[:1]:
             try:
-                # 会社評価ポイントの取得
-                # parseDate = textList[site].find(dateTag[site][0], class_=dateTag[site][1])
-                # cpoint = parseDate.getText().replace('\n', '').replace(' ', '')
-                # #会社名の取得
-                # parseCname =  textList[site].find(nameTag[site][0], class_=nameTag[site][1])
-                # cname = parseCname.getText().replace('\n','').replace(' ', '')
+                # parse name and time
                 parseTable = textList[site].find(table[site][0], id=table[site][1])
-                time = re.findall('\d{4}-\d{2}-\d{2} \d+:\d+:\d+',str(parseTable))
-                # time = parseTable.findAll('time')
-                # a_tag = parseTable.findAll('a')
-                # link_element = a_tag['href']
-                # for i in range(len(time)):
-                #     href = 	link_element['href']
-                # contest_name = parseTable.findAll('a')
-                contest_name = re.findall('contests[\w\d/%#$&?()~_.=+-]+',str(parseTable))
-                # contest_name_str = contest_name.string
+                time = re.findall('\d{4}-\d{2}-\d{2} \d+:\d+:\d+',str(parseTable))#from <time>
+                contest_name = re.findall('contests[\w\d/%#$&?()~_.=+-]+',str(parseTable))#from <a href=...>
 
-                cTable = parseTable.getText().replace('\n','').split()
-                # parse_contest = textList[site].find(contest_table)
-                # cContest = parse_contest.getText().split('\n')
-
-
-                # print(parseTable,'\n')
-                # print(time)
-                # print(contest_name)
-
-            # 検索結果が無かった場合の処理
+            # 検索結果が無かった場合の処理 #if nothing find in table
             except AttributeError:
                 contest_name_time = None,None
 
 
-            # 検索結果が有った場合の処理
+            # if find elements
         for i in range(len(time)):
             str_time.append(datetime.datetime.strptime(time[i], '%Y-%m-%d %H:%M:%S'))
             # print(str_time)
-
             modified_2H.append(str_time[i] - datetime.timedelta(hours=2))
-
-            contest_name_time += contest_name[i] + ':\n' + str(modified_2H[i]) +'(KHtime +0700)\n\n'
+            # contest_name_time += contest_name[i] + ':\n' + str(modified_2H[i]) +'(KHtime +0700)\n\n'
+            contest_name_time.append(contest_name[i] + ' ' + str(modified_2H[i]) + '(KHtime +0700)')
 
         # print(modified_2H)
         return contest_name_time
 
 # print(GetHtml().getText())
-print(ParseHtml().parseNamePoint(GetHtml().getText()))
+print(ParseHtml().parseName_Date(GetHtml().getText()))
